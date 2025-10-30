e5d0d9570160da834e70d5d533126279557d203f
Miftau <miftaudeenb@gmail.com>
2025-10-30 17:14:51 +0100

    fixing tooltip

--- src/hooks/useTooltip.ts
+++ src/hooks/useTooltip.ts
@@ -4,7 +4,7 @@ import Map from 'ol/Map';
 import Point from 'ol/geom/Point';
 
 export const useTooltip = (
-  mapInstance: RefObject<Map | null>,
+mapInstance: RefObject<Map | null>, tooltipOverlayRef: any,
 ) => {
   useEffect(() => {
     if (!mapInstance.current) return;
